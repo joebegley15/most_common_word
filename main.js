663aafdfcbbddcf71e4110550ccec22277fc8441
Joe Begley <joebegley15@gmail.com>
2018-08-24 12:23:45 -0400

    Solution added

---
+++ main.js
@@ -0,0 +1,13 @@
+function mostCommonWord(str) {
+	var words = [], wordCount = {};
+	words = str.match(/[a-z]+/gi);
+	console.log(words);
+	words.map(function(word){
+		word = word.toLowerCase();
+		wordCount[word] = wordCount[word] + 1 || 1;
+	})
+	console.log(wordCount);
+	return Object.keys(wordCount).reduce((currentMax,element) => wordCount[element] > wordCount[currentMax] ? element : currentMax);
+}
+
+console.log(mostCommonWord('I am a string, my most mostCommonWord is string. Trust me.'));
